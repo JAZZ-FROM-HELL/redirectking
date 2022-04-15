9eeea687f13f27cb18353228c6e104696227a189
yuval <yuval.ron.services@gmail.com>
2022-04-15 07:06:45 +0200

    init commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const useragent = require('express-useragent');
+
+const app = express();
+app.use(useragent.express());
+
+app.get('/ig', (req, res) => {
+    console.log(req.useragent);
+    if (req.useragent.isAndroid) {
+        res.redirect(301, 'intent://www.instagram.com/yuval.ron.music#Intent;package=com.instagram.android;scheme=https;end');
+    }
+    else if (req.useragent.isiPhone || req.useragent.isiPad || req.useragent.isiPhoneNative || req.useragent.isiPod) {
+        res.redirect(301, 'instagram://user?username=yuval.ron.music');
+    }
+    else {
+        res.redirect(301, 'https://www.instagram.com/yuval.ron.music');
+    }
+});
+
+app.listen(4000, () => console.log('Listening on port 4000!'));
+
